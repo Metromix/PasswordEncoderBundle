1c10ad7535ec3f45ea488a8228876983105f31e6
Dominik Labudzinski <dominik@labudzinski.com>
2017-10-04 10:27:05 +0200

    travis

--- .travis-libsodium.sh
+++ .travis-libsodium.sh
@@ -2,4 +2,4 @@
 sudo add-apt-repository --yes ppa:chris-lea/libsodium;
 sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
 sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
-sudo apt-get update && sudo apt-get install libsodium;
+sudo apt-get update && sudo apt-get install libsodium-dev;
